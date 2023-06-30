efee9fba04b15a96d5bb2d23f5724901071de0de
s481809 <wlakuc@st.amu.edu.pl>
2023-06-30 16:17:11 +0200

    Add eslint and prettier to the project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import "bootstrap/dist/css/bootstrap.css";
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
+import 'bootstrap/dist/css/bootstrap.css'
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
 
-createApp(App).mount("#app");
+createApp(App).mount('#app')
 
-import "bootstrap/dist/js/bootstrap.js";
+import 'bootstrap/dist/js/bootstrap.js'
