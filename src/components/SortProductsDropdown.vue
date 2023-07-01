9b4bb68c96ea9f6c34254c654da742e417b53a23
wladyslaw-kuczerenko <wladyslaw.kuczerenko@wundermanthompson.com>
2023-07-01 14:08:44 +0200

    * Create separate Dropdown component for sorting products needs * Move redundant things from header to more appropriate place

---
+++ src/components/SortProductsDropdown.vue
@@ -0,0 +1,36 @@
+<template>
+    <BaseDropdown :options="dropdownOptions" @select="setSelectedSortOption" />
+</template>
+
+<script setup lang="ts">
+import BaseDropdown from './BaseDropdown.vue'
+import { useProductsStore } from '@/stores/products'
+
+const productsStore = useProductsStore()
+
+const dropdownOptions = [
+    {
+        value: 'ascending',
+        text: 'Najtańsze',
+    },
+    {
+        value: 'descending',
+        text: 'Najdroższe',
+    },
+]
+const setSelectedSortOption = (value: string) => {
+    switch (value) {
+        case 'descending':
+            productsStore.setSortBy(value)
+            break
+
+        case 'ascending':
+            productsStore.setSortBy(value)
+            break
+
+        default:
+            console.warn('Unknown value in selectedSortOption', value)
+            break
+    }
+}
+</script>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,28 +1,12 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand fw-bold" href="#">Shopware listing</a>
-            <BaseDropdown :options="dropdownOptions" @select="setSelectedSortOption" />
+            <h5 class="navbar-brand fw-bold mb-0">Shopware listing</h5>
+            <SortProductsDropdown />
         </div>
     </nav>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import BaseDropdown from './BaseDropdown.vue'
-
-const selectedSortOption = ref<string | null>(null)
-const dropdownOptions = [
-    {
-        value: 'ascending',
-        text: 'Najtańsze',
-    },
-    {
-        value: 'descending',
-        text: 'Najdroższe',
-    },
-]
-const setSelectedSortOption = (value: string) => {
-    selectedSortOption.value = value
-}
+import SortProductsDropdown from './SortProductsDropdown.vue'
 </script>
